dceb5dad26afbd3b9d39e195bea57f16e42d2fc4
Javad Shafique <javadshafique@hotmail.com>
2021-10-12 14:55:15 +0200

    :(

--- config.js
+++ config.js
@@ -1,7 +1,11 @@
+const dotenv = require('dotenv');
+
+dotenv.config();
+
 module.exports = {
     port: process.env.PORT || 1337,
     admin: {
-        username: 'admin',
-        password: '2314120iedqwojkdowed'
+        username: process.env.user,
+        password: process.env.pass
     }
 }
