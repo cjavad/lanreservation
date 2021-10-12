8c5309e5bc1b2f3b8b8445bc5695c1d8bf59b913
Javad Shafique <javadshafique@hotmail.com>
2021-10-12 14:52:47 +0200

    Auth now in .env, + more styling.

--- app.js
+++ app.js
@@ -30,8 +30,8 @@ const ID = () => {
 const GO_BACK_HTML = `<a href="/">Go back</a>`;
 
 app.get("/panel", [
-    query('username').equals(config.username).withMessage('Username is incorrect'),
-    query('password').equals(config.password).withMessage('Password is incorrect'),
+    query('username').equals(config.admin.username).withMessage('Username is incorrect'),
+    query('password').equals(config.admin.password).withMessage('Password is incorrect'),
 ], (req, res) => {
     const errors = validationResult(req);
 
@@ -47,8 +47,8 @@ app.get("/", (req, res) => {
 });
 
 app.post("/get_access", [
-    body('username').equals(config.username).withMessage('Username is incorrect'),
-    body('password').equals(config.password).withMessage('Password is incorrect'),
+    body('username').equals(config.admin.username).withMessage('Username is incorrect'),
+    body('password').equals(config.admin.password).withMessage('Password is incorrect'),
     body('name').notEmpty().withMessage('Name is required')
 ], (req, res) => {
     const errors = validationResult(req);
@@ -69,8 +69,28 @@ app.post("/get_access", [
 
     res.send(`
         <html>
-            <p>${req.body.name}</p>
-            <a href="https://lanreservation.javad.ninja/?id=${id}">https://lanreservation.javad.ninja/?id=${id}</a>
+            <head>
+                <meta charset="UTF-8">
+                <meta http-equiv="X-UA-Compatible" content="IE=edge">
+                <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                <title>LANreservaTION Vibenshus Gymnasium Fall 2021</title>
+                <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
+                    integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+                <link href="http://fonts.googleapis.com/css?family=Corben:bold" rel="stylesheet" type="text/css">
+                <link href="http://fonts.googleapis.com/css?family=Nobile" rel="stylesheet" type="text/css">
+                <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
+                    integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p"
+                    crossorigin="anonymous"></script>
+                <link rel="stylesheet" href="style.css">
+            </head>
+            <body>
+                <div class="container" style="background: white; color:">
+                    <p>${req.body.name}</p>
+                    <a href="https://lanreservation.javad.ninja/?id=${id}">https://lanreservation.javad.ninja/?id=${id}</a>
+                    <br>
+                    <a href="/panel?username=${req.body.username}&password=${req.body.password}">Go back</a>
+                </div>
+            </body>
         </html>
     `);
 });
